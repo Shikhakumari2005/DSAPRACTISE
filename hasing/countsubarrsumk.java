fc3d19bc627c3bbd6a896090c7a69eb4e3f1906f
unknown <siragenius1@gmail.com>
2026-02-21 23:45:00 +0530

    countsubarrofsumk

---
+++ hasing/countsubarrsumk.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class countsubarrsumk {
+    public static int count(int[] arr,int tar){
+        int count=0;
+        int sum=0;
+        HashMap<Integer,Integer>mp=new HashMap();
+        for (int i = 0; i < arr.length; i++) {
+            sum +=arr[i];
+            if(mp.containsKey(sum==tar)){
+                count +=mp.get(sum);
+            }
+            mp.put(sum,1);
+        }
+        return count;
+       
+    }
+    public static void main(String[] args) {
+        int[] arr={2,3,5,4,4,1};
+        System.out.println(count(arr,8 ));
+
+    }
+}
--- hasing/maxminfreofarr.java
+++ hasing/maxminfreofarr.java
@@ -1,33 +1,87 @@
+import java.sql.Time;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class maxminfreofarr {
-    public static int[] printmaxmin(int[] arr){
+    //public static int[] printmaxmin(int[] arr){
     //Brute force
-          int maxelmt=0; 
-          int minelmt=0;
-          int maxfq=0;
-          int minfrq=arr.length;
+        //   int maxelmt=0; 
+        //   int minelmt=0;
+        //   int maxfq=0;
+        //   int minfrq=arr.length;
 
-          for(int i=0;i<arr.length;i++){
-                int c=1;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    c++;
+        //   for(int i=0;i<arr.length;i++){
+        //         int c=1;
+        //     for(int j=i+1;j<arr.length;j++){
+        //         if(arr[i]==arr[j]){
+        //             c++;
+        //         }
+        //     }
+        //     if(maxfq<c){
+        //         maxfq=c;
+        //         maxelmt=arr[i];
+
+        //     }
+        //     if(minfrq>c){
+        //         minfrq=c;
+        //         minelmt=arr[i];
+
+        //     }
+        //   }
+        //   return new int[] {maxelmt,minelmt, maxfq,minfrq};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //optimizes way
+
+    public static int[] printmaxmin(int[] arr){
+         // Step 1: Count frequency
+            HashMap<Integer,Integer>mp=new HashMap<>();
+            for(int i=0;i<arr.length;i++){
+                int num=arr[i];
+                if(mp.containsKey(num)){
+                    mp.put(num,mp.get(num)+1);
+                }else{
+                    mp.put(num, 1);
                 }
             }
-            if(maxfq<c){
-                maxfq=c;
-                maxelmt=arr[i];
+        // Step 2: Find max and min frequency
+        int maxFreq = 0;
+        int minFreq = arr.length;
+        int maxEle = -1;
+        int minEle = -1;
 
-            }
-            if(minfrq>c){
-                minfrq=c;
-                minelmt=arr[i];
+        for (int key : mp.keySet()) {
+            int freq = mp.get(key);
 
+            if (freq > maxFreq) {
+                maxFreq = freq;
+                maxEle = key;
             }
-          }
-          return new int[] {maxelmt,minelmt, maxfq,minfrq};
+
+            if (freq < minFreq) {
+                minFreq = freq;
+                minEle = key;
+            }
+        }
+
+        // return: [maxElement, minElement]
+        return new int[]{maxEle, minEle};
     }
+
+  
     public static void main(String[] args){
         int[] arr={2,3,3,2,4,3,5,2,6,3,3};
         int[] res=printmaxmin(arr);

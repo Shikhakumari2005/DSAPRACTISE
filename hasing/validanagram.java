6e36f6fb2ff3e797f9c51e935cc62555f1cc6ab4
unknown <siragenius1@gmail.com>
2026-02-25 23:20:51 +0530

    validanagram

---
+++ hasing/validanagram.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class validanagram {
+    public boolean isanagram(int[] a,int[] b){
+        HashMap<Integer,Integer>c=new HashMap<>();
+        for(int i=0; i<a)
+    }
+}

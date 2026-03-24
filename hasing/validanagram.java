e30f0fa89ecbaa2235b9206cb8b60d73c67f3133
unknown <siragenius1@gmail.com>
2026-03-24 13:52:37 +0530

    Modified Aligram

--- hasing/validanagram.java
+++ hasing/validanagram.java
@@ -1,8 +1,30 @@
-import java.util.HashMap;
+//import java.util.HashMap;
+import java.util.*;
+ 
 
-public class validanagram {
-    public boolean isanagram(int[] a,int[] b){
-        HashMap<Integer,Integer>c=new HashMap<>();
-        for(int i=0; i<a)
+class validAnagram{
+    public static boolean ValidAnagram(String str1,String str2){
+        char[] ch1=str1.toCharArray();
+        char[] ch2=str2.toCharArray();
+        if(ch1.length!=ch2.length){
+            return false;
+        }
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+        for(int i=0; i<ch1.length;i++){
+            if(ch1[i]!=ch2[i]){
+                return false;
+            }
+            
+        }
+        // }
+        // else{
+        return true;
+        // }
     }
-}
+    public static void main(String[] args) {
+        String str1="face";
+        String str2="acef";
+        System.out.println(ValidAnagram(str1, str2));
+    }
+}   

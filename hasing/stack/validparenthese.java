1497c18748a7c3bcd6d1a8e1a2ca89f41eb535f1
unknown <siragenius1@gmail.com>
2026-03-27 13:21:42 +0530

    validparenthese

---
+++ hasing/stack/validparenthese.java
@@ -0,0 +1,33 @@
+package stack;
+
+import java.util.Stack;
+
+public class validparenthese {
+    public static Stack<Character> validpara(String str){
+        Stack<Character> st=new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch=str.charAt(i);
+            if(ch=='[' || ch=='(' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if (st.isEmpty()) {
+                    return st; // or false
+                }
+                st.peek();
+                if(st.peek()=='(' && ch==')' || st.peek()=='[' && ch==']' || st.peek()=='{' && ch=='}'){
+                    st.pop();
+                }
+                else{
+                   System.out.println("invalid" + ch);
+                }
+            }
+            
+        }
+        return st;
+    }
+    public static void main(String[] args) {
+        String str="{[({[})]}";
+        System.out.println(validpara(str));
+    }
+}

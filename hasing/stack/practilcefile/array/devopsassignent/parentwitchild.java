07e4240e186731559cc0ffed771e5a4cd3b03940
Your Name <your@email.com>
2026-05-25 16:36:35 +0530

    Updated parentwitchild.java

--- hasing/stack/practilcefile/array/devopsassignent/parentwitchild.java
+++ hasing/stack/practilcefile/array/devopsassignent/parentwitchild.java
@@ -6,7 +6,11 @@ class parentwithchild{
     public static void main(String[] args){
         Scanner scr=new  Scanner(System.in);
         int n=scr.nextInt();                 //Reads the number of nodes.
-        ArrayList<Integer>[] k=new ArrayList[n+1];        //create array of arraylist
+        ArrayList<Integer>[] k=new ArrayList[n+1]; 
+               //create array of arraylist  and n+1 is used because nodes are numbered from 1
+
+
+        //Creates an empty list for every node.
         for (int i = 0; i <=n; i++) {
             k[i]=new ArrayList<>();
         }
@@ -15,9 +19,11 @@ class parentwithchild{
             int x=scr.nextInt();
             int y=scr.nextInt();
             k[x].add(y);
-            k[y].add(x);
+            k[y].add(x);          //Add y to x's neighbor list.  Because the graph is undirected.
             i++;
         }
+
+        //bfs intialization
         Queue<Integer>q=new LinkedList<>();
         int[] use=new int[n+1];
         use[1]=1;
@@ -25,7 +31,7 @@ class parentwithchild{
 
         while(!q.isEmpty()){
             int node =q.poll();
-            int c=0;
+            int c=0;               //Counts unvisited children of current node.
             for(int u :k[node]){
                 if(use[u]==0){
                     c++;

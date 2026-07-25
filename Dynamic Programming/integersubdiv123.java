1d2313b1ec52b5eb81155110307ddf691a0ddf9e
Your Name <siragenius1@gmail.com>
2026-07-25 23:44:17 +0530

    longestsubarrwithoutrepetition

--- Dynamic Programming/integersubdiv123.java
+++ Dynamic Programming/integersubdiv123.java
@@ -1,9 +1,9 @@
 public class integersubdiv123 {
     public static int[] MinumstepNto1(int N){
         
-        int[] dp=new int[N];
+        int[] dp=new int[N +1];
         dp[1]=0;
-        for(int i=2; i<N;i++){
+        for(int i=2; i<=N;i++){
             dp[i]=dp[i-1] +1;
             if(i%2==0){
                 dp[i]=Math.min(dp[i], dp[i/2] +1);
@@ -19,5 +19,7 @@ public class integersubdiv123 {
         for(int val:ans){
             System.out.print(val);
         }
+        System.out.println("\nMinimum steps for " + N + " = " + ans[N]);
+    
     }
 }
